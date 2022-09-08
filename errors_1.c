55ce1fcc3accbffaf980b17bfe86135c79356b1e
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-09-09 00:01:42 +0200

    Update errors_1.c

--- errors_1.c
+++ errors_1.c
@@ -1,68 +1,94 @@
 #include "monty.h"
-
-int usage_error(void);
-int malloc_error(void);
-int f_open_error(char *filename);
-int unknown_op_error(char *opcode, unsigned int line_number);
-int no_int_error(unsigned int line_number);
-
 /**
- * usage_error - Prints usage error messages.
- *
- * Return: (EXIT_FAILURE) always.
+ * _add - adds the top two elements in stack
+ * @h: top node
+ * @line_number: node number
  */
-int usage_error(void)
+void _add(stack_t **h, unsigned int line_number)
 {
-	fprintf(stderr, "USAGE: monty file\n");
-	return (EXIT_FAILURE);
+	if (*h == NULL || (*h)->next == NULL)
+	{
+		printf("L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*h)->next->n += (*h)->n;
+	(*h) = (*h)->next;
+	free((*h)->prev);
+	(*h)->prev = NULL;
 }
-
 /**
- * malloc_error - Prints malloc error messages.
- *
- * Return: (EXIT_FAILURE) always.
+ * _sub - subtract second node from top node
+ * @h: top of list
+ * @line_number: line of command
  */
-int malloc_error(void)
+void _sub(stack_t **h, unsigned int line_number)
 {
-	fprintf(stderr, "Error: malloc failed\n");
-	return (EXIT_FAILURE);
+	if (*h == NULL || (*h)->next == NULL)
+	{
+		printf("L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*h)->next->n -= (*h)->n;
+	(*h) = (*h)->next;
+	free((*h)->prev);
+	(*h)->prev = NULL;
 }
-
 /**
- * f_open_error - Prints file opening error messages w/ file name.
- *
- * @filename: Name of file failed to open
- * Return: (EXIT_FAILURE) always
+ * _mul - multiply second node and top node
+ * @h: top of list
+ * @line_number: line of command
  */
-
-int f_open_error(char *filename)
+void _mul(stack_t **h, unsigned int line_number)
 {
-	fprintf(stderr, "Error: Can't open file %s\n", filename);
-	return (EXIT_FAILURE);
+	if (*h == NULL || (*h)->next == NULL)
+	{
+		printf("L%u: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*h)->next->n *= (*h)->n;
+	(*h) = (*h)->next;
+	free((*h)->prev);
+	(*h)->prev = NULL;
 }
-
 /**
- * unknown_op_error - Prints unknown instruction error messages.
- * @opcode: Opcode where error occurred.
- * @line_number: Line number in Monty bytecodes file where error occured.
- *
- * Return: (EXIT_FAILURE) always
+ * _div - divide second node from top node
+ * @h: top of list
+ * @line_number: line of command
  */
-int unknown_op_error(char *opcode, unsigned int line_number)
+void _div(stack_t **h, unsigned int line_number)
 {
-	fprintf(stderr, "L%u: unknown instruction %s\n",
-			line_number, opcode);
-	return (EXIT_FAILURE);
+	if (*h == NULL || (*h)->next == NULL)
+	{
+		printf("L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*h)->n == 0 || (*h)->next->n == 0)
+	{
+		printf("L%u: division by zero\n", line_number);
+	}
+	(*h)->next->n /= (*h)->n;
+	(*h) = (*h)->next;
+	free((*h)->prev);
+	(*h)->prev = NULL;
 }
-
 /**
- * no_int_error - Prints invalid monty_push argument error messages.
- * @line_number: Line number in Monty bytecodes file where error occurred.
- *
- * Return: (EXIT_FAILURE) always
+ * _mod - mod second node from top node
+ * @h: top of list
+ * @line_number: line of command
  */
-int no_int_error(unsigned int line_number)
+void _mod(stack_t **h, unsigned int line_number)
 {
-	fprintf(stderr, "L%u: usage: push integer\n", line_number);
-	return (EXIT_FAILURE);
+	if (h == NULL || *h == NULL)
+	{
+		printf("L%u: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*h)->n == 0 || (*h)->next->n == 0)
+	{
+		printf("L%u: division by zero\n", line_number);
+	}
+	(*h)->next->n %= (*h)->n;
+	(*h) = (*h)->next;
+	free((*h)->prev);
+	(*h)->prev = NULL;
 }
